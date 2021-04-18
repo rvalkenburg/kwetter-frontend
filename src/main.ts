c69841c87405d50d2c41477cca72a51649f90c0a
Roger Valkenburg <I412512@fhict.nl>
2021-04-18 16:44:39 +0200

    installed dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import App from './App.vue';
 //Bootstrap for styling
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import installElementPlus from './plugins/element'
+import store from './store'
 
-
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(store).use(router);
+app.use(router);
+app.mount('#app');

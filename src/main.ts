c61d0a90c31ba070c4a16e1d5d9819503a7c854f
Roger Valkenburg <I412512@fhict.nl>
2021-06-11 20:56:27 +0200

    Update paths

--- src/main.ts
+++ src/main.ts
@@ -28,12 +28,12 @@ import interceptorsSetup from './services/HttpCommon.ts'
 interceptorsSetup()
 
 const firebaseConfig = {
-  apiKey: process.env.VUE_APP_APIKEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-  appId: process.env.VUE_APP_APP_ID
+  apiKey: process.env.ENVIRONMENTAL_VARIABLES_VUE_APP_APIKEY,
+  authDomain: process.env.ENVIRONMENTAL_VARIABLES_VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.ENVIRONMENTAL_VARIABLES_VUE_APP_PROJECT_ID,
+  storageBucket: process.env.ENVIRONMENTAL_VARIABLES_VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.ENVIRONMENTAL_VARIABLES_VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.ENVIRONMENTAL_VARIABLES_VUE_APP_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig);
